25d02335df88f378f52fa1f2a30fd97d6b036350
vihaan365 <vk080212@gmail.com>
2025-08-03 15:53:00 +0530

    lesson19

---
+++ lesson19/script.js
@@ -0,0 +1,18 @@
+console.log("hello world")
+
+var c = 70
+var lordvoldemort =  90
+var a = c * lordvoldemort
+
+var b = 100
+var d = 600
+console.log(a)
+console.log(a*b)
+console.log(a*b+c/d)
+
+var e = 1000
+e += 189
+e *= 3
+e /= 2
+e -= 200
+console.log(e)
