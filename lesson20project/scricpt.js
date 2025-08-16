5ed5f7ffeb80af30c589571275eb35d1a26d87b2
vihaan365 <vk080212@gmail.com>
2025-08-16 14:18:14 +0530

    lesson20project

---
+++ lesson20project/scricpt.js
@@ -0,0 +1,18 @@
+var a = 90;
+var b = 98;
+
+function multiplication(){
+    ("the example of subtration is 98 -90 and the answer is " + (d*a));
+}
+
+
+multiplication();
+
+var c = 900;
+var d = 450;
+
+function multiply(a, b, c, d){
+    var res = (a * b) / (c * d);
+    return res;
+
+}
