9352cdb1e72e9075a5c566b2d99d9bb4840c8559
vihaan365 <vk080212@gmail.com>
2025-08-30 15:57:50 +0530

    lesson25

---
+++ lesson25/script.js
@@ -0,0 +1,47 @@
+var a = [49/7 + 21];
+var b = "leon is the best pokemon trainer"; 
+var c = true;
+var d = false;
+var e = null;
+var f = undefined;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+
+
+
+var g = "the best football player in the is messi";
+g = "the best player in the world is ronaldo"; 
+console.log(g);
+
+var f = 90;
+f = 104040;
+f= 710
+console.log(f);
+
+if(a>90){
+    console.log("a is grater than 90");
+}
+if(a>400){
+    console.log("a is greater than 400");
+}
+else {
+    console.log("a is less than 400");
+}
+if(a>90){
+    console.log("a is greater than 100000");
+}
+if(a>90){
+    console.log("a is greater than 5000000");}
+else {
+    console.log("a is less than 90 , 100000 , 5000000")
+}
+
+let i = 0;
+for(i=0 ; i<100 ; i++) {
+    console.log('harry kane is in loop');
+}
