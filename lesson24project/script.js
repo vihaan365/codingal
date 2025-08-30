b1e77cef839cb96049a2799ec9c46d58179feaec
vihaan365 <vk080212@gmail.com>
2025-08-30 09:17:01 +0530

    lesson24project

---
+++ lesson24project/script.js
@@ -0,0 +1,15 @@
+const e1 = document.getElementsByClassName("pals");
+e1[1].style.backgroundColor = "blue";
+console.log(e1);
+
+const e2s = document.getElementsByTagName("span");
+e2s[2].style.backgroundColor = "green";
+
+const e3s = document.getElementById("chaizard");
+console.log(e3s);
+
+const e4s = document.querySelector(".pals");
+console.log(e4s);
+
+const e5s = document.querySelectorAll("span");
+console.log(e5s);
