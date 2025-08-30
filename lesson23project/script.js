a59f20bfde71b51d915f052d7052679ce1609438
vihaan365 <vk080212@gmail.com>
2025-08-30 08:08:13 +0530

    lesson23project

---
+++ lesson23project/script.js
@@ -0,0 +1,17 @@
+var b = {"marcelo , ronldo , benzema , kroos , ramos , modric , beckham" };
+
+console.log(b[4]);
+console.log(b.length);
+console.log(b.length-6);
+
+b.forEach(element => console.log(element));
+
+function sum(){
+    return a+c+d;
+}
+function average(){
+    return(a+c+d)/3;
+}
+
+var avg = average(7,80,-100);
+console.log(avg);
