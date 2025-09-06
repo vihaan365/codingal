9822bedd2f264b83e482b9fcb5bb0356eea331a7
vihaan365 <vk080212@gmail.com>
2025-09-06 13:04:10 +0530

    lesson25project

---
+++ lesson25project/script.js
@@ -0,0 +1,43 @@
+var a = [49/7 + 21];
+var b = "real madrid is the best football club";
+var c = true;
+var d = false;
+var e = null;
+var f = undefined;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+
+
+var g = "the best magician in the world is lord voldemort";
+g = "the best wizard in the world is albus dumbledore";
+console.log(g);
+
+var f = 50;
+f = 1020304050;
+f = -1020304050;
+console.log(f);
+
+if(a>45){
+    console.log("a is greater than 45");
+}
+if(a<2000000){
+    console.log("a is less than 2000000");
+}
+else{
+    console.log("a is greater than 2000000");
+}
+if(a>-200000000){
+    console.log("a is greater than -200000000");
+}
+else{
+    console.log("a is less than - 200000000");
+}
+let i = 0;
+for(z=0 ; z<100 ; z++){
+    console.log("PORTUGAL will win the FIFA worldcup in 2026");
+}
