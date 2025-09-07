0e54cafc04ff3eb8d0e3ef04c722c7f43597d3a4
vihaan365 <vk080212@gmail.com>
2025-09-07 15:46:01 +0530

    lesson28

---
+++ lesson28/script.js
@@ -0,0 +1,41 @@
+var a = "hello world";
+var b = 928;
+var c = 7.10;
+var d = true;
+var e = false;
+var f = "586";
+
+console.log(typeof(c));
+
+c = Boolean(c);
+
+console.log(c);
+console.log(typeof(c));
+
+console.log(typeof(f));
+f = Number(f);
+
+console.log(f);
+console.log(typeof(f));
+
+console.log(typeof(a));
+a = Number(a)
+
+console.log(a);
+console.log(typeof(a));
+
+d = Number(d);
+e = Number(e);
+
+console.log(d);
+console.log(e);
+
+b = "my finger is talking rubbish today it says that you must be sleepy but you are not sleepy because sleepy is not you and you are not  sleepy so should not be sleepy because you are not sleppy and sleepy is not you";
+ m = a.search("sleepy");
+
+ console.log(m);
+
+ function az (a){
+    return a*100;
+ }
+var aaz = a => a*100;
