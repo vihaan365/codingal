61bc13cc6c93911f555b5a634fcef433689a5818
vihaan365 <vk080212@gmail.com>
2025-11-01 18:02:17 +0530

    python

---
+++ python/demo.py
@@ -0,0 +1,17 @@
+animal = 'cnhjfrhiuqwehchehehefih'
+print(animal)
+print(type(animal))
+
+animal1 = 123456789
+animal2 = 123.45
+animal3 = True
+animal4 = None
+animal5 = [1, 2, 3, 4, 5]
+print(animal3)
+print(type(animal3))
+
+
+#this is a comment
+
+"""this is a
+multiline comment"""
