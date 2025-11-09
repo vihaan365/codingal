2e34f17c52654e56de63cb929ee7c774717f7e23
vihaan365 <vk080212@gmail.com>
2025-11-09 15:41:47 +0530

    loops

---
+++ python/loops.py
@@ -0,0 +1,26 @@
+
+# for loop
+
+for i in range(1,6) :
+    print( i)
+
+# while loop
+
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+# nested loop
+for i in range(1,6):
+    for j in range(2,7):
+        print(i,j)
+
+name = "vihaan"
+for i in range(5):
+    print(name)
+
+    
+    
+    
+    
