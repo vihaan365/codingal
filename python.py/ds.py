dca38d03a0f45c5f5e61f6e2edbcb586d5c37759
vihaan365 <vk080212@gmail.com>
2025-11-29 18:42:14 +0530

    python

---
+++ python.py/ds.py
@@ -0,0 +1,21 @@
+arr=[ 'name', 'age', 'gender', 'city'] 
+print(len(arr))
+
+print(arr[0])
+print(arr[-1])
+arr.append('country')
+print(arr)
+arr.remove('age')
+print(arr)
+arr.sort()
+print(arr)
+arr.pop(2)
+print(arr)
+arr.reverse()
+print(arr)
+arr*2
+print(arr)
+array=['a', 'b', 'c', 'd', 'e']
+print(array [:len(array)-1])
+arr.clear()
+print(arr)
