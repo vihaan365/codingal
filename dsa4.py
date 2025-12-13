d95a528510971b5acaa1fa0fc5cbf0febab60aa1
vihaan365 <vk080212@gmail.com>
2025-12-13 17:38:26 +0530

    dsa4

---
+++ dsa4.py
@@ -0,0 +1,22 @@
+a = {}
+ 
+dic = {"name" = john, "age" = 25, "city" = "New York"}
+
+#update value
+dic["age"] = 26
+
+#add item
+dic["job"] = "Engineer"
+
+#remove item
+dic.pop("city")
+
+#print dictionary
+print(dic)
+
+
+
+
+
+
+
